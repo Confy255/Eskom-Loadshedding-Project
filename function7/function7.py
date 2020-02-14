4f7e7c55ebd1ca00dbe66601e8064874993afc65
Sandile Dladla <sdladlad@gmail.com>
2020-02-14 14:05:40 +0200

    Added version-1 of function 7

--- function7/function7.py
+++ function7/function7.py
@@ -6,3 +6,14 @@ def stop_words_remover(df):
         
     list_of_list_of_tweets = [i.split() for i in list_of_tweets if i not in final_list_of_tweets]
     stop_words_list = stop_words_dict['stopwords']
+    for i in list_of_list_of_tweets:
+        for p in i:
+            if p in stop_words_list:
+                i.remove(p)
+            else:
+                continue
+    
+    df_final_list_of_tweets = pd.DataFrame({'Without Stop Words': list_of_list_of_tweets})
+    final_df = pd.concat([df,df_final_list_of_tweets], axis=1)
+
+    return final_df
