596cc61c31bdac35c210156cebdfaa6961a3423c
KaraboLeso <karabopleso@gmail.com>
2020-02-14 12:17:37 +0200

    updated, final function1(dictionery metrics)

--- metric_dictionery/function_1.py
+++ metric_dictionery/function_1.py
@@ -3,8 +3,8 @@ import numpy as numpy
 def dictionary_of_metrics(items):
     mean = round(np.mean(items), 2)
     median = round(np.median(items), 2)
-    variance = round(np.var(items, ddof=1),2)
-    standard_deviation = round(np.std(items, ddof=1), 2)
+    variance = round(np.var(items, ddof= 1),2)
+    standard_deviation = round(np.std(items, ddof= 1), 2)
     minimum = round(min(items), 2)
     maximum = round(max(items), 2)
      
