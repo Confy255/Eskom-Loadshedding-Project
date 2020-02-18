ca59fcb4a1923a8f5818e3e7ca6e4d876d0f306a
KaraboLeso <karabopleso@gmail.com>
2020-02-18 12:44:11 +0200

    import numpy, and the data

--- five_number_summary/func_2.py
+++ five_number_summary/func_2.py
@@ -1,21 +1,20 @@
-if n%2 == 0:
-    hlf_evn1 = int(n/2)
-    upper_hlf_evn = x[hlf_evn1:]
-    n_upper_hlf_evn = len(upper_hlf_evn)
-    if n_upper_hlf_evn%2 == 0:
-        t = int((n_upper_hlf_evn/2)-1)
-        Q3 = (upper_hlf_evn[t] + upper_hlf_evn[t+1])/2
-    else:
-        i = int((n_upper_hlf_evn+1)/2)
-        Q3 = upper_hlf_evn[i-1]
-else:
-    hlf_odd1 = int((n+1)/2)
-    upper_hlf_odd = x[hlf_odd1:]
-    n_upper_hlf_odd = len(upper_hlf_odd) 
-    if n_upper_hlf_odd%2 == 0:
-        e = int((n_upper_hlf_odd/2)-1)
-        Q3 = (upper_hlf_odd[e] + upper_hlf_odd[e+1])/2
-    else:
-        o = int((n_upper_hlf_odd+1)/2)
-        Q3 = upper_hlf_odd[o-1]
-    break
+import numpy as np
+
+ebp_url = 'https://raw.githubusercontent.com/Explore-AI/Public-Data/master/Data/electrification_by_province.csv'
+ebp_df = pd.read_csv(ebp_url)
+
+for col, row in ebp_df.iloc[:,1:].iteritems():
+    ebp_df[col] = ebp_df[col].str.replace(',','').astype(int)
+
+ebp_df.head()
+
+
+### START FUNCTION
+def five_num_summary(items):
+    maximum = round(max(items),2)
+    median = round(np.median(items),2)
+    minimum = round(min(items),2)
+    Q1 = round(np.percentile(items, 25), 2)
+    Q3 = round(np.percentile(items, 75), 2)
+    return {'max': maximum, 'median': median, 'min': minimum, "q1":Q1, "q3":Q3}
+### END FUNCTION
