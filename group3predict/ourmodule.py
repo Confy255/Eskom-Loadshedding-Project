0743ac5eb37d442b68ae9827725fadb28ef112ec
KaraboLeso <karabopleso@gmail.com>
2020-02-18 15:36:17 +0200

    fuction 1 version 1

--- group3predict/ourmodule.py
+++ group3predict/ourmodule.py
@@ -64,3 +64,22 @@ stop_words_dict = {
         'same', 'were', 'it', 'every', 'third', 'together'
     ]
 }
+
+#Function 1
+
+def dictionary_of_metrics(items):
+    mean = round(np.mean(items), 2)
+    median = round(np.median(items), 2)
+    var = round(np.var(items, ddof=1), 2)
+    std_dev = round(np.std(items, ddof=1), 2)
+    minimum = round(min(items), 2)
+    maximum = round(max(items), 2)
+
+    return {
+        'mean': mean,
+        'median': median,
+        'var': var,
+        'std': std_dev,
+        'min': minimum,
+        'max': maximum
+        }
--- metric_dictionery/function_1.py
+++ metric_dictionery/function_1.py
@@ -1,17 +1,2 @@
-import numpy as np
-def dictionary_of_metrics(items):
-    mean = round(np.mean(items), 2)
-    median = round(np.median(items), 2)
-    var = round(np.var(items, ddof=1), 2)
-    std_dev = round(np.std(items, ddof=1), 2)
-    minimum = round(min(items), 2)
-    maximum = round(max(items), 2)
 
-    return {
-        'mean': mean,
-        'median': median,
-        'var': var,
-        'std': std_dev,
-        'min': minimum,
-        'max': maximum
-        }
+
