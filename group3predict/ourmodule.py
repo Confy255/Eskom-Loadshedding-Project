aea1dab7e1350bcc7be93a90cbadcfd86bfde42a
Confy255 <60347718+Confy255@users.noreply.github.com>
2020-02-19 09:28:47 +0200

    Added comments to function5

--- group3predict/ourmodule.py
+++ group3predict/ourmodule.py
@@ -206,9 +206,12 @@ def number_of_tweets_per_day(df):
     """This function retuns a new dataframe that has
         a counted number of tweets per given date
     """
-    
+    #modify date to yyyy-mm-dd format
     mod_date = [i.split(' ', 1)[0] for i in dates]
+
     twitter_df = pd.read_csv(twitter_url)
+
+    #create new dataframe that counts number of tweets, grouped by mod_date
     twitter_df_by_tweets= twitter_df.groupby(mod_date)['Tweets'].count()
     new_dataframe = pd.DataFrame(twitter_df_by_tweets).rename_axis('Date')
 
